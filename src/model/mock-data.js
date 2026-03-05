1b603883fa603cac2fbab314fb7f048a14b2af9d
Ildar <spulaemulae98@gmail.com>
2026-03-05 06:57:16 +0500

    mock data dest img fix

--- src/model/mock-data.js
+++ src/model/mock-data.js
@@ -3,14 +3,16 @@ import Offer from './offer-model.js';
 import Point from './point-model.js';
 import { PointType } from './const.js';
 
+// Моковые данные для пунктов назначения с локальными картинками
 export const mockDestinations = [
   new Destination(
     'dest-1',
     'Chamonix',
     'Chamonix, is a beautiful city, a true asian pearl, with crowded streets.',
     [
-      { src: 'http://picsum.photos/300/200?r=0.0762563005163317', description: 'Chamonix parliament building' },
-      { src: 'http://picsum.photos/300/200?r=0.0762563005163318', description: 'Chamonix mountain view' }
+      { src: 'img/photos/1.jpg', description: 'Chamonix mountain view' },
+      { src: 'img/photos/2.jpg', description: 'Chamonix town center' },
+      { src: 'img/photos/3.jpg', description: 'Chamonix cable car' }
     ]
   ),
   new Destination(
@@ -18,8 +20,9 @@ export const mockDestinations = [
     'Geneva',
     'Geneva is a city in Switzerland that lies at the southern tip of expansive Lac Léman.',
     [
-      { src: 'http://picsum.photos/300/200?r=0.1762563005163317', description: 'Geneva lake' },
-      { src: 'http://picsum.photos/300/200?r=0.2762563005163317', description: 'Geneva fountain' }
+      { src: 'img/photos/2.jpg', description: 'Geneva lake' },
+      { src: 'img/photos/4.jpg', description: 'Geneva fountain' },
+      { src: 'img/photos/5.jpg', description: 'Geneva old town' }
     ]
   ),
   new Destination(
@@ -27,12 +30,24 @@ export const mockDestinations = [
     'Amsterdam',
     'Amsterdam is the capital city of the Netherlands, known for its artistic heritage and canal system.',
     [
-      { src: 'http://picsum.photos/300/200?r=0.3762563005163317', description: 'Amsterdam canal' },
-      { src: 'http://picsum.photos/300/200?r=0.4762563005163317', description: 'Amsterdam museums' }
+      { src: 'img/photos/3.jpg', description: 'Amsterdam canal' },
+      { src: 'img/photos/1.jpg', description: 'Amsterdam museums' },
+      { src: 'img/photos/5.jpg', description: 'Amsterdam bicycles' }
+    ]
+  ),
+  new Destination(
+    'dest-4',
+    'Berlin',
+    'Berlin, Germany\'s capital, dates to the 13th century. Reminders of the city\'s turbulent 20th-century history include its Holocaust memorial and the Berlin Wall\'s graffitied remains.',
+    [
+      { src: 'img/photos/4.jpg', description: 'Berlin wall' },
+      { src: 'img/photos/2.jpg', description: 'Brandenburg gate' },
+      { src: 'img/photos/1.jpg', description: 'Reichstag building' }
     ]
   )
 ];
 
+// Остальные моковые данные остаются без изменений...
 export const mockOffers = [
   new Offer('offer-1', 'Upgrade to a business class', 120, PointType.TAXI),
   new Offer('offer-2', 'Add luggage', 50, PointType.TAXI),
@@ -61,12 +76,12 @@ export const mockPoints = [
     '2019-07-10T22:55:56.845Z',
     '2019-07-11T11:22:13.375Z',
     false,
-    ['offer-1', 'offer-2'] 
+    ['offer-1', 'offer-2']
   ),
   new Point(
     'point-2',
     PointType.FLIGHT,
-    'dest-2', 
+    'dest-2', // Geneva
     2500,
     '2019-07-12T08:30:00.000Z',
     '2019-07-12T13:45:00.000Z',
@@ -76,7 +91,7 @@ export const mockPoints = [
   new Point(
     'point-3',
     PointType.TRAIN,
-    'dest-3', 
+    'dest-3', // Amsterdam
     850,
     '2019-07-13T14:20:00.000Z',
     '2019-07-13T18:10:00.000Z',
@@ -86,7 +101,7 @@ export const mockPoints = [
   new Point(
     'point-4',
     PointType.BUS,
-    'dest-1',
+    'dest-4', // Berlin
     350,
     '2019-07-14T09:15:00.000Z',
     '2019-07-14T12:30:00.000Z',
@@ -96,7 +111,7 @@ export const mockPoints = [
   new Point(
     'point-5',
     PointType.SHIP,
-    'dest-2', 
+    'dest-2', // Geneva
     1800,
     '2019-07-15T10:00:00.000Z',
     '2019-07-15T16:30:00.000Z',
@@ -105,20 +120,10 @@ export const mockPoints = [
   )
 ];
 
+// Вспомогательные функции
 export const getDestinations = () => mockDestinations;
-
 export const getOffers = () => mockOffers;
-
-export const getOffersByType = (type) => {
-  return mockOffers.filter(offer => offer.type === type);
-};
-
+export const getOffersByType = (type) => mockOffers.filter(offer => offer.type === type);
 export const getPoints = () => mockPoints;
-
-export const getDestinationById = (id) => {
-  return mockDestinations.find(dest => dest.id === id);
-};
-
-export const getOffersByIds = (offerIds) => {
-  return mockOffers.filter(offer => offerIds.includes(offer.id));
-};
+export const getDestinationById = (id) => mockDestinations.find(dest => dest.id === id);
+export const getOffersByIds = (offerIds) => mockOffers.filter(offer => offerIds.includes(offer.id));
