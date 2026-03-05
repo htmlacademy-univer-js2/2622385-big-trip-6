946e1aa82c9359ba03acc5cd58a9defd443c3cab
Ildar <spulaemulae98@gmail.com>
2026-03-05 06:53:19 +0500

    trip-presenter update

--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -1,57 +1,77 @@
 import FiltersView from '../view/filters-view.js';
 import SortingView from '../view/sorting-view.js';
 import PointView from '../view/point-view.js';
-import AddNewPointView from '../view/add-new-point-view.js'; // Форма создания
-import PointEditView from '../view/point-edit-view.js';      // Форма редактирования
-
-const POINT_COUNT = 3
+import PointEditView from '../view/point-edit-view.js';
+import TripModel from '../model/trip-model.js';
 
 export default class TripPresenter {
   constructor() {
     this.container = null;
     this.filtersContainer = null;
+    this.model = new TripModel();
   }
 
   init() {
     this.container = document.querySelector('.trip-events');
     this.filtersContainer = document.querySelector('.trip-controls__filters');
-
+    
     if (!this.container) {
       console.error('Container .trip-events not found!');
       return;
     }
 
-    this.container.innerHTML = '';
+    this.render();
+  }
 
+  render() {
+    this.container.innerHTML = '';
+    
     this.renderFilters();
 
     const sortingView = new SortingView();
     this.container.appendChild(sortingView.getElement());
 
+    const points = this.model.getPoints();
+    const destinations = this.model.getDestinations();
+    const offers = this.model.getOffers();
+
+    if (points.length === 0) {
+      this.container.innerHTML += '<h2 class="trip-events__title">No events</h2>';
+      return;
+    }
+
     const eventsList = document.createElement('ul');
     eventsList.classList.add('trip-events__list');
     this.container.appendChild(eventsList);
 
-    const pointEditView = new PointEditView();
-    eventsList.appendChild(pointEditView.getElement());
-
-    for (let i = 0; i < POINT_COUNT; i++) {
-      const pointView = new PointView();
-      eventsList.appendChild(pointView.getElement());
+    if (points.length > 0) {
+      const firstPoint = points[0];
+      const destination = this.model.getDestinationById(firstPoint.destinationId);
+      const pointOffers = this.model.getOffersByIds(firstPoint.offerIds);
+      
+      const editView = new PointEditView(
+        firstPoint, 
+        destinations, 
+        (type) => this.model.getOffersByType(type),
+        offers
+      );
+      eventsList.appendChild(editView.getElement());
     }
+
+    points.slice(1).forEach(point => {
+      const destination = this.model.getDestinationById(point.destinationId);
+      const pointOffers = this.model.getOffersByIds(point.offerIds);
+      
+      const pointView = new PointView(point, destination, pointOffers);
+      eventsList.appendChild(pointView.getElement());
+    });
   }
 
   renderFilters() {
     if (this.filtersContainer) {
       const filtersView = new FiltersView();
-      this.filtersContainer.innerHTML = ''; 
+      this.filtersContainer.innerHTML = '';
       this.filtersContainer.appendChild(filtersView.getElement());
     }
-  }
-
-  createNewPoint() {
-    const eventsList = this.container.querySelector('.trip-events__list');
-    const addNewPointView = new AddNewPointView();
-    eventsList.prepend(addNewPointView.getElement()); 
   }
 }
