bcf98e0a631bebe33d7cfeb4463ac9d76ab3da7f
Ildar <spulaemulae98@gmail.com>
2026-05-21 01:37:54 +0500

    trip-presenter, sorting view edit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
+import TripModel from './model/trip-model.js';
 import TripPresenter from './presenter/trip-presenter.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const tripPresenter = new TripPresenter();
+  const tripModel = new TripModel();
+  const tripPresenter = new TripPresenter(tripModel);
   tripPresenter.init();
 });
--- src/view/sorting-view.js
+++ src/view/sorting-view.js
@@ -1,4 +1,4 @@
-import AbstractView from '../framework/view/abstract-view';
+import AbstractView from '../framework/view/abstract-view.js';
 
 export const SortType = {
   DAY: 'day',
@@ -8,39 +8,104 @@ export const SortType = {
   OFFER: 'offer'
 };
 
-function sortingTemplate() {
+function createSortingTemplate() {
   return `
-      <form class="trip-events__trip-sort  trip-sort" action="#" method="get">
+    <form class="trip-events__trip-sort  trip-sort" action="#" method="get">
       <div class="trip-sort__item  trip-sort__item--day">
-        <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day" checked>
+        <input 
+          id="sort-day" 
+          class="trip-sort__input  visually-hidden" 
+          type="radio" 
+          name="trip-sort" 
+          value="${SortType.DAY}" 
+          checked
+          data-sort-type="${SortType.DAY}"
+        >
         <label class="trip-sort__btn" for="sort-day">Day</label>
       </div>
 
       <div class="trip-sort__item  trip-sort__item--event">
-        <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event" disabled>
+        <input 
+          id="sort-event" 
+          class="trip-sort__input  visually-hidden" 
+          type="radio" 
+          name="trip-sort" 
+          value="${SortType.EVENT}" 
+          disabled
+          data-sort-type="${SortType.EVENT}"
+        >
         <label class="trip-sort__btn" for="sort-event">Event</label>
       </div>
 
       <div class="trip-sort__item  trip-sort__item--time">
-        <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
+        <input 
+          id="sort-time" 
+          class="trip-sort__input  visually-hidden" 
+          type="radio" 
+          name="trip-sort" 
+          value="${SortType.TIME}" 
+          data-sort-type="${SortType.TIME}"
+        >
         <label class="trip-sort__btn" for="sort-time">Time</label>
       </div>
 
       <div class="trip-sort__item  trip-sort__item--price">
-        <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
+        <input 
+          id="sort-price" 
+          class="trip-sort__input  visually-hidden" 
+          type="radio" 
+          name="trip-sort" 
+          value="${SortType.PRICE}" 
+          data-sort-type="${SortType.PRICE}"
+        >
         <label class="trip-sort__btn" for="sort-price">Price</label>
       </div>
 
       <div class="trip-sort__item  trip-sort__item--offer">
-        <input id="sort-offer" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-offer" disabled>
+        <input 
+          id="sort-offer" 
+          class="trip-sort__input  visually-hidden" 
+          type="radio" 
+          name="trip-sort" 
+          value="${SortType.OFFER}" 
+          disabled
+          data-sort-type="${SortType.OFFER}"
+        >
         <label class="trip-sort__btn" for="sort-offer">Offers</label>
       </div>
-      </form>
-    `;
+    </form>
+  `;
 }
 
 export default class SortingView extends AbstractView {
-  get template() {
-    return sortingTemplate();
+  #currentSortType = SortType.DAY;
+  #handleSortTypeChange = null;
+
+  constructor() {
+    super();
   }
+
+  get template() {
+    return createSortingTemplate();
+  }
+
+  setSortTypeChangeHandler(callback) {
+    this.#handleSortTypeChange = callback;
+    this.element.addEventListener('change', this.#sortTypeChangeHandler);
+  }
+
+  #sortTypeChangeHandler = (evt) => {
+    if (evt.target.tagName !== 'INPUT') {
+      return;
+    }
+
+    const sortType = evt.target.dataset.sortType;
+
+    if (sortType === this.#currentSortType) {
+      return;
+    }
+
+    this.#currentSortType = sortType;
+    this.#handleSortTypeChange?.(sortType);
+  };
 }
